ec226efd5de89cacf86ab8c9f9cc150b8b9ff5bb
Phalcin <Phalcinhayes@gmail.com>
2024-04-26 22:56:43 +0000

    new changes

---
+++ app.js
@@ -0,0 +1,11 @@
+let password;
+
+if (password === 8) {
+  console.log("Welcome");
+} else if (password <= 8) {
+  console.log("Password is too short");
+} else if (password >= 8) {
+  console.log("Password too long");
+} else {
+  console.log("please provide password");
+}
