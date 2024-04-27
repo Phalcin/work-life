ba88c1423887e328f80705fab7a8772856ff1a91
Phalcin <Phalcinhayes@gmail.com>
2024-04-27 12:01:03 +0000

    new changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import Add from "./Add";
 
+const myName = "Emmanuel";
+const multiply = (a, b) => a * b;
+const specialClass = "simple-class";
+
 function App() {
   return (
-    <section>
-      <Add />
-    </section>
+    <>
+      <h1>{myName}</h1>
+      <p>{2 + 2}</p>
+      <p>My friends list: {["Alex", " John", " Jordan"]}</p>
+    </>
   );
 }
 
