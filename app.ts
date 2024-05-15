d120fb4bffea33a05341efc61306cfe179d420ba
Phalcin <Phalcinhayes@gmail.com>
2024-05-15 15:24:41 +0000

    new changes

--- app.ts
+++ app.ts
@@ -1,15 +1,29 @@
-interface Song {
-  songName: string;
-  singerName: string;
-  printSongInfo(songName: string, singerName: string): string;
-}
+import express, { Express, Response, Request } from 'express';
 
-const song1: Song = {
-  songName: "Natural",
-  singerName: "Imagine Dragon",
-  printSongInfo: (songName, singerName) => {
-    return `Song: ${songName}, Singer: ${singerName}`;
-  },
-};
-console.log(song1.printSongInfo("Natural", "imagine Dragon"));
-console.log(song1.songName);
+app.get('/menu/list', function (req: Request, res: Response) {
+  const requestQuery: { id: number[] | number } | any = req.query;
+  const foundMenus = [];
+  const validIds: number[] = [34, 45, 23, 23, 4];
+
+  if (typeof requestQuery.id === 'string') {
+    // parse the id to integer and do what is necessary
+    const parseId: number = parseInt(requestQuery.id);
+  } else {
+    for (let id of requestQuery.id) {
+      const parseId: number = parseInt(id);
+      const _: number | undefined = validIds.find(
+        (validId) => validId === parseId,
+      );
+
+      if (!_) {
+        return res
+          .status(404)
+          .send({ message: `Menu of id ${parseId} does not exist!` });
+      }
+
+      foundMenus.push({ name: 'banku', id: 3 }, { name: 'beans', id: 5 });
+    }
+  }
+
+  res.status(200).send({ message: foundMenus });
+});
